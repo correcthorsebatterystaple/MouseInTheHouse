3610d8f08e8a2ba34e8499ccca4d2fee46e08673
Robert McCann <bogoblin@gmail.com>
2019-01-26 23:20:21 +0000

    Draw the grid

--- mith.js
+++ mith.js
@@ -7,30 +7,59 @@ let width  = canvas.width;
 let height = canvas.height;
 
 let mouse = {
-    x: 50,
-    y: 50
+    x: 14*32,
+    y: 40*32
 };
 let currentLevel;
+
+const collisionlayer = 0;
+function isSolid(x, y, space="screen") {
+    if (space=="screen") {
+        if (currentLevel.layers[collisionlayer].tiles[Math.floor(x/32) + Math.floor(y/32)*currentLevel.width]) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (space=="tile") {
+        if (currentLevel.layers[collisionlayer].tiles[x + y*currentLevel.width]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 
 function loadLevel(path) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             currentLevel = JSON.parse(this.responseText);
+            camera.draw();
         }
     };
-    xmlhttp.open("GET", "https://github.com/correcthorsebatterystaple/MouseInTheHouse/blob/master/"+path, true);
+    xmlhttp.open("GET", "https://raw.githubusercontent.com/correcthorsebatterystaple/MouseInTheHouse/master/"+path, true);
     xmlhttp.send();
 }
 
-function drawTile(xt, yt, xoff, yoff) {
-    let tile = currentLevel.tiles[xt + yt*currentLevel.width];
+function drawTile(xt, yt, xoff, yoff, layer) {
+    let tile = currentLevel.layers[layer].tiles[xt + yt*currentLevel.width];
+    console.log(`${layer} ${xt + yt*currentLevel.width}`);
+    if (!tile) return;
     let x = xt*32 - xoff;
     let y = yt*32 - yoff;
     let id = tile.id;
-    let sx = (tile.id % 8) * 32;
-    let sy = Math.floor(tile.id/8);
-    ctx.drawImage(tilemap, sx, sy, 32, 32, x, y);
+    console.log(tile.id);
+    let sx = (id % 8) * 32;
+    let sy = Math.floor(id/8)*32;
+    ctx.drawImage(tilemap, sx, sy, 32, 32, x, y, 32, 32);
+}
+
+for (let id=0; id<9; id++) {
+    let x = 0;
+    let y = 0;
+    let sx = (id % 8) * 32;
+    let sy = Math.floor(id/8);
+    ctx.drawImage(tilemap, sx, sy, 32, 32, x, y, 32, 32);
 }
 
 class Camera {
@@ -46,7 +75,9 @@ class Camera {
         let tny = Math.ceil(height/32)+1;
         for (let yt=y1t; yt<=y1t+tny; yt++) {
             for (let xt=x1t; xt<=x1t+tnx; xt++) {
-                drawTile(xt, yt, xoff, yoff);
+                for (let layer = 0; layer < currentLevel.layers.length; layer++) {
+                    drawTile(xt, yt, xoff, yoff, layer);
+                }
             }
         }
     }
