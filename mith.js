60b1b7604c1ff05fc45b939e737dcc1405c6c308
Robert McCann <bogoblin@gmail.com>
2019-01-27 00:01:46 +0000

    Add collision layer

--- mith.js
+++ mith.js
@@ -6,11 +6,38 @@ const tilemap = document.getElementById("tilemap");
 let width  = canvas.width;
 let height = canvas.height;
 
-let mouse = {
-    x: 14*32,
-    y: 40*32
-};
+let mouse = new Mouse({x: 14*32, y:40*32}, {x:0,y:0});
 let currentLevel;
+
+let controls = {
+    left: false,
+    right: false,
+    up: false,
+    down: false,
+    jump: false
+}
+canvas.addEventListener('keydown', event => {
+    if (event.keycode == 97) {
+        controls.left = true;
+    } else if (event.keycode == 100) {
+        controls.right = true;
+    } else if (event.keycode == 119) {
+        controls.up = true;
+    } else if (event.keycode == 115) {
+        controls.down = true;
+    }
+});
+canvas.addEventListener('keyup', event => {
+    if (event.keycode == 97) {
+        controls.left = false;
+    } else if (event.keycode == 100) {
+        controls.right = false;
+    } else if (event.keycode == 119) {
+        controls.up = false;
+    } else if (event.keycode == 115) {
+        controls.down = false;
+    }
+});
 
 const collisionlayer = 0;
 function isSolid(x, y, space="screen") {
@@ -21,7 +48,7 @@ function isSolid(x, y, space="screen") {
             return false;
         }
     } else if (space=="tile") {
-        if (currentLevel.layers[collisionlayer].tiles[x + y*currentLevel.width]) {
+        if (currentLevel.layers[collisionlayer].tiles[x%currentLevel.width + y*currentLevel.width]) {
             return true;
         } else {
             return false;
@@ -29,12 +56,12 @@ function isSolid(x, y, space="screen") {
     }
 }
 
-function loadLevel(path) {
+function loadLevel(path, callback) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             currentLevel = JSON.parse(this.responseText);
-            camera.draw();
+            callback();
         }
     };
     xmlhttp.open("GET", "https://raw.githubusercontent.com/correcthorsebatterystaple/MouseInTheHouse/master/"+path, true);
@@ -42,7 +69,7 @@ function loadLevel(path) {
 }
 
 function drawTile(xt, yt, xoff, yoff, layer) {
-    let tile = currentLevel.layers[layer].tiles[xt + yt*currentLevel.width];
+    let tile = currentLevel.layers[layer].tiles[xt%currentLevel.width + yt*currentLevel.width];
     console.log(`${layer} ${xt + yt*currentLevel.width}`);
     if (!tile) return;
     let x = xt*32 - xoff;
@@ -54,28 +81,21 @@ function drawTile(xt, yt, xoff, yoff, layer) {
     ctx.drawImage(tilemap, sx, sy, 32, 32, x, y, 32, 32);
 }
 
-for (let id=0; id<9; id++) {
-    let x = 0;
-    let y = 0;
-    let sx = (id % 8) * 32;
-    let sy = Math.floor(id/8);
-    ctx.drawImage(tilemap, sx, sy, 32, 32, x, y, 32, 32);
-}
-
 class Camera {
     constructor() {
     }
 
     draw() {
-        let xoff = mouse.x - width/2;
-        let yoff = mouse.y - height/2;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        let xoff = mouse.position.x - width/2;
+        let yoff = mouse.position.y - height/2;
         let x1t = Math.floor(xoff/32);
         let y1t = Math.floor(yoff/32);
         let tnx = Math.ceil(width/32)+1;
         let tny = Math.ceil(height/32)+1;
         for (let yt=y1t; yt<=y1t+tny; yt++) {
             for (let xt=x1t; xt<=x1t+tnx; xt++) {
-                for (let layer = 0; layer < currentLevel.layers.length; layer++) {
+                for (let layer = 1; layer < currentLevel.layers.length; layer++) {
                     drawTile(xt, yt, xoff, yoff, layer);
                 }
             }
@@ -83,8 +103,17 @@ class Camera {
     }
 }
 
-loadLevel("./testlevel.json");
+function update() {
+    camera.draw();
+    mouse.draw();
+    requestAnimationFrame(update);
+}
+
+loadLevel("./testlevel.json", () => {
+    update();
+});
 const camera = new Camera();
+
 
 
 /*
